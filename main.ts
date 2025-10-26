3c07a69b02caa2cb6bd828fc8e05ddbe914a0700
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-10-26 19:48:53 -0400

    fixed reset

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Tiago
+ * Created on: oct 2025
+ * This program plays cookie clicker.
 */
 
-basic.showString('Hello, World!')
+//setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// variables
+let currentNumber : number = 0
+
+// add 1 to currentNumber
+input.onButtonPressed(Button.A, function() {
+    // add 1
+    basic.clearScreen()
+    currentNumber = currentNumber + 1
+    basic.showNumber(currentNumber)
+})
+
+// reset currentNumber
+input.onButtonPressed(Button.B, function() {
+    // reset to 0
+    currentNumber = currentNumber - currentNumber
+    basic.showNumber(currentNumber)
+})
+
+
