790c60d7ceaa771c5300ef4bec4493fc03a4b3db
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-10-27 13:09:23 -0400

    reput the code

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: Tiago baker
+ * Created on: Sep 2025
  * This program ...
 */
 
-basic.showString('Hello, World!')
+/* Copyright (c) 2020 MTHS All rights reserved
+ *
+ * Created by: Tiago Baker
+ * Created on: Oct 2025
+ * This program plays Cookie Clicker.
+*/
+
+//setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// variables
+let currentNumber: number = 0
+
+// add 1 to currentNumber
+input.onButtonPressed(Button.A, function () {
+    // add 1
+    basic.clearScreen()
+    currentNumber = currentNumber + 1
+    basic.showNumber(currentNumber)
+})
+
+// reset currentNumber
+input.onButtonPressed(Button.B, function () {
+    // reset to 0
+    currentNumber = currentNumber - currentNumber
+    basic.showNumber(currentNumber)
+})
